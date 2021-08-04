c5a6bcf4ce281451e655e92b9c5bcd077397c3f7
LyubovP <lyubovpidoshva@gmail.com>
2021-08-04 11:15:03 +0000

    Finish signup

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -1,4 +1,27 @@
 class CouriersController < ApplicationController
-  def new
+  
+  def show
+    @courier = Courier.find(params[:id])
   end
+  
+  def new
+    @courier = Courier.new
+  end
+  
+  def create
+    @courier = Courier.new(courier_params)    
+    if @courier.save
+      flash[:success] = "Welcome to the Delivery App!"
+       redirect_to @courier
+       
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def courier_params
+      params.require(:courier).permit(:name, :email)
+    end
 end
